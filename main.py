37f979c7706dc04300a4227fc48aac1985d145f3
Alexey <64686526+monsterJuk@users.noreply.github.com>
2024-08-13 17:10:17 +0500

    final merge

--- main.py
+++ main.py
@@ -5,8 +5,3 @@ spot = Spot(api_key=API_KEY, api_secret=API_SECRET)
 
 print(spot.ping())
 print(spot.time())
-<<<<<<< HEAD
-print("Goodbye")
-=======
-print('Hello')
->>>>>>> 15af0aca44a38cf3d28525d448a82e007eee8e38
