ff98a9cd618ca58953fa07a32ba1ba83de23f5ee
monster <musakovich@gmail.com>
2024-08-13 17:02:42 +0500

    test merge1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from  config import API_KEY, API_SECRET
+from config import API_KEY, API_SECRET
 from mexc_sdk import Spot
 
 spot = Spot(api_key=API_KEY, api_secret=API_SECRET)
 
 print(spot.ping())
 print(spot.time())
-spot.
+print("Goodbye")
