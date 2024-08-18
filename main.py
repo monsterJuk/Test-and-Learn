046126dbaf70c02648fda4827461434df5db65fc
monster <musakovich@gmail.com>
2024-08-18 11:15:47 +0500

    first_test_request

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from config import API_KEY, API_SECRET
-from mexc_sdk import Spot
+import requests
 
-spot = Spot(api_key=API_KEY, api_secret=API_SECRET)
+base_url = 'https://contract.mexc.com/'
+endpoints = {
+    'server_time': 'api/v1/contract/ping',
+    'info': 'api/v1/private/account/assets'
+}
 
-print(spot.ping())
-print(spot.time())
+url = base_url + endpoints.get('info')
+headers = {'ApiKey': API_KEY}
+
+response = requests.get(url=url, headers=headers)
+print(response.json())
